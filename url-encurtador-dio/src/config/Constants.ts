b97cfb64cab75c8496f78fdefe065ddfbc8e22a6
Victor Bruno Silva Barbosa <84999168+victor21k@users.noreply.github.com>
2021-11-27 21:43:16 -0300

    Add files via upload

---
+++ url-encurtador-dio/src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://victor21l:<password>@url-encurtador-dio.t0asi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
